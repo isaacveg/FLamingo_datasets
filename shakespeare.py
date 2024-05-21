75b50bbc39affe00f8ee9856d212137919d6e55e
isaacveg <isaaczhu@mail.ustc.edu.cn>
2024-05-21 18:51:45 +0800

    Finish Shakespeare dataset

--- shakespeare.py
+++ shakespeare.py
@@ -1,4 +1,5 @@
 # Generate Shakespeare dataset for Federated Learning settings.
+import shutil
 from torchvision import datasets, transforms
 import json
 import torch
@@ -28,6 +29,7 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=2048, help='random seed')
     parser.add_argument('--indir', type=str, default='./utils/leaf_scripts/shakespeare/data/', help='input dataset directory')
     parser.add_argument('--outdir', type=str, default='../datasets/', help='output dataset directory')
+    parser.add_argument('-mt', '--minimum_test_samples', type=int, default=0, help='minimum number of test samples per client')
     # usage: bash ./preprocess.sh -s niid --sf 0.2 -k 0 -t sample -tf 0.8
     # non-iid, select 20% percent of clients, keep clients with at least 0 samples, train-test split 80-20
     # Make sure to delete the rem_user_data, sampled_data, test, and train subfolders in the data directory before re-running preprocess.sh
@@ -45,14 +47,16 @@ if __name__ == '__main__':
     outdir = os.path.join(outdir, f'shakespeare')
     train_dir = os.path.join(outdir, 'train')
     test_dir = os.path.join(outdir, 'test')
-    if not os.path.exists(outdir):
-        os.makedirs(train_dir, exist_ok=True)
-        os.makedirs(test_dir, exist_ok=True)
-    else:
+    if os.path.exists(outdir):
         print("Dataset already generated. Do you want to overwrite it? (y/n)")
         ans = input()
         if ans.lower() != 'y':
             exit(0)
+        else:
+            shutil.rmtree(outdir)
+    os.makedirs(train_dir, exist_ok=True)
+    os.makedirs(test_dir, exist_ok=True)
+
         
     # suppose you have already downloaded the dataset 
     # and placed it in the input directory
@@ -68,10 +72,10 @@ if __name__ == '__main__':
     # we use ['user_data'] and ['x'], ['y'] to transform the data
     with open(train_file) as f:
         raw_train_data = json.load(f)
-        print(raw_train_data['num_samples'])
+        # print(raw_train_data['num_samples'])
     with open(test_file) as f:
         raw_test_data = json.load(f)
-        print(raw_test_data['num_samples'])
+        # print(raw_test_data['num_samples'])
 
     train_data_ = []
     train_data_len = []
@@ -83,7 +87,7 @@ if __name__ == '__main__':
         train_data_len.append(len(train_data_[-1]['x']))
     i=0
     for k, v in raw_test_data['user_data'].items():
-        print(i:=i+1 ,len(v['x']), len(v['y']))
+        # print(i:=i+1 ,len(v['x']), len(v['y']))
         test_data_.append({'x': process_x(v['x']), 'y': process_y(v['y'])})
         test_data_len.append(len(test_data_[-1]['x']))
         # print(len(test_data_[-1]['x']))
@@ -98,19 +102,23 @@ if __name__ == '__main__':
         # print(train_data_len[ind], test_data_len[ind], len(train_data[-1]['x']), len(test_data[-1]['x']))
     
     stats = {}
-    for idx, train_dict in enumerate(train_data):
+    idx = 0
+    for i, train_dict in enumerate(train_data):
         # for NCP tasks, each client should record stats including:
         # train_size, test_size
+        if len(test_data[i]['y']) < args.minimum_test_samples:
+            continue
         stats[idx+1] = {}
         stats[idx+1]['train_size'] = len(train_dict['y'])
-        stats[idx+1]['test_size'] = len(test_data[idx]['y'])
+        stats[idx+1]['test_size'] = len(test_data[i]['y'])
         np.savez_compressed(os.path.join(train_dir, f'{idx+1}.npz'), 
                             data=train_dict['x'], 
                             targets=train_dict['y'])
         np.savez_compressed(os.path.join(test_dir, f'{idx+1}.npz'), 
                             data=test_data[idx]['x'], 
                             targets=test_data[idx]['y'])
+        idx += 1
         
-    save_stats_json('shakespeare', stats, outdir, seed=seed, dist='niid', num_clients=len(train_data))
+    save_stats_json('shakespeare', stats, outdir, seed=seed, dist='niid', num_clients=idx)
     print(f"Data saved in {outdir}")
     print("Dataset generated successfully!")
